9c667717015cc04c6f671ab259a95339d0ce9ac4
ryakosh <ryakosh@gmail.com>
2018-10-26 14:57:31 +0330

    Change flex to inline-flex

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -17,7 +17,7 @@ export default {
   background-color: white;
   border-radius: 13px;
   box-shadow: 0px 0px 6px rgba(0, 0, 0, .5);
-  display: flex;
+  display: inline-flex;
   justify-content: center;
   align-items: center;
 }
