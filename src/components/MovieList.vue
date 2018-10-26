c7fb5a0367c6a28eea33f630838063158e8c6eee
ryakosh <ryakosh@gmail.com>
2018-10-26 14:57:58 +0330

    Add pop-movie-list component

---
+++ src/components/MovieList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="movie-list">
+    <h2 class="movie-list__name">{{ name }}</h2>
+    <div class="movie-list__container">
+      <pop-movie-card v-for="(movie, i) of subMovies" :movie="movie" :key="i" />
+    </div>
+  </div>
+</template>
+
+<script>
+import MovieCard from './MovieCard.vue';
+
+export default {
+  name: 'pop-movie-list',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+    movies: {
+      type: Array,
+      required: true,
+    },
+    limit: {
+      type: Number,
+      required: false,
+      default: 10,
+    },
+  },
+  components: {
+    'pop-movie-card': MovieCard,
+  },
+  computed: {
+    subMovies() {
+      return this.movies.slice(0, this.limit);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.movie-list {
+  &__name {
+    margin-left: 9.8px;
+  }
+  &__container {
+    padding-bottom: 2px;
+    margin: 13.8px 0 0 18.8px;
+    overflow-x: auto;
+    display: flex;
+  }
+
+  .card {
+    margin-right: 13.9px;
+  }
+}
+</style>
