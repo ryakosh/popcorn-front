d7a62921e8fa2174f834d04471a4c1ff08796976
ryakosh <ryakosh@gmail.com>
2018-11-03 18:39:25 +0330

    Add pop-rate-button-circle component

---
+++ src/components/RateButtonCircle.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="rate-button-circle" @click="hClick">{{ value }}</div>
+</template>
+
+<script>
+export default {
+  name: 'pop-rate-button-circle',
+  props: {
+    value: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    hClick() {
+      this.$emit('on-click', this.value);
+    },
+  },
+};
+</script>
+
+<style>
+.rate-button-circle {
+  width: 14.9px;
+  height: 14.9px;
+  border-radius: 50%;
+  background-color: white;
+  box-shadow: 0 0 3px rgba(0, 0, 0, .65);
+  font-size: 9px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
