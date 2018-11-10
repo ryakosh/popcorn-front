05a64b15449af32e519318ac37befb92422d6266
ryakosh <ryakosh@gmail.com>
2018-11-10 14:59:07 +0330

    Add Card component test

---
+++ src/components/__tests__/Card.test.js
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils';
+import Card from '../Card.vue';
+
+describe('Card component', () => {
+  it("mounts injected content", () => {
+    const w = shallowMount(Card, {
+      slots: {
+        default: 'test',
+      },
+    });
+    expect(w.text()).toBe('test');
+  });
+});
