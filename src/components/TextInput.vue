397b25db8ceda4514c74b6450242b8c859cc75a5
ryakosh <ryakosh@gmail.com>
2018-11-12 20:03:40 +0330

    Add TextInput component

---
+++ src/components/TextInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <input class="text-input" type="text" :value="value" @input="hInput" />
+</template>
+
+<script>
+export default {
+  name: 'pop-text-input',
+  props: ['value'],
+  methods: {
+    hInput(e) {
+      this.$emit('input', e.target.value);
+    },
+  },
+};
+</script>
+
+<style>
+.text-input {
+  min-width: 200px;
+  width: auto;
+  height: 44px;
+  padding: 0 10px;
+  border: none;
+  border-radius: 20px;
+  outline: none;
+  background-color: white;
+  box-shadow: 0 0 5px rgba(0, 0, 0, .65);
+}
+</style>
