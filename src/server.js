faa3bb61ab648c6d645abd0e602107ac20ef8cda
ryakosh <ryakosh@gmail.com>
2018-11-21 20:45:00 +0330

    Add signup method

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import qs from 'qs';
+
+class Server {
+  constructor(xConfig = { /* Default axios config */ }) {
+    this.x = axios.create(xConfig);
+    this.cache = {};
+  }
+
+  signup(email, uname, pwd) {
+    const data = qs.stringify({ email, uname, pwd })
+    return this.x.post('/signup', data)
+  }
+}server
