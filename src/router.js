b25ea81586917434bb37a8bd0ba7bb38069d5972
ryakosh <ryakosh@gmail.com>
2018-11-23 14:43:51 +0330

    Add Signup route

--- src/router.js
+++ src/router.js
@@ -1,9 +1,14 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+import Signup from './views/Signup.vue';
+
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
+  routes: [
+    { path: '/signup', component: Signup }
+  ],
 });
