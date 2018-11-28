2c221dfe257ba11daf2574e8ef8830a59cf4d291
ryakosh <ryakosh@gmail.com>
2018-11-28 18:31:18 +0330

    Add \signin route

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import Router from "vue-router";
 
+// Views
 import Signup from './views/Signup.vue';
+import Signin from './views/Signin.vue';
 
 Vue.use(Router);
 
@@ -9,6 +11,7 @@ export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
-    { path: '/signup', component: Signup }
+    { path: '/signup', component: Signup },
+    { path: '/signin', component: Signin },
   ],
 });
