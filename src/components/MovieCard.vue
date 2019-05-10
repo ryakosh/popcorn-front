86a7148840b5a8c5f4cd953c3fc70e9fb3b1a033
ryakosh <ryakosh@gmail.com>
2019-05-10 11:47:06 +0430

    Remove MovieScore component

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -3,20 +3,17 @@
     <div class="movie-card">
       <img class="movie-card__poster" :src="movie.poster">
       <span class="movie-card__title">{{ movie.title }}</span>
-      <pop-movie-score :score="movie.score" />
     </div>
   </pop-card>
 </template>
 
 <script>
 import Card from './Card.vue';
-import MovieScore from './MovieScore.vue';
 
 export default {
   name: 'pop-movie-card',
   components: {
     'pop-card': Card,
-    'pop-movie-score': MovieScore,
   },
   props: {
     movie: {
@@ -47,10 +44,6 @@ export default {
     background-color: #2C3E50;
     font-size: 10px;
     color: white;
-  }
-  &__score {
-    margin-top: 6.9px;
-    margin-left: 4px;
   }
 }
 </style>
