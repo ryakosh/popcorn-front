1bf2c5156d9e6069a28eab7d1a65d604587764d5
ryakosh <ryakosh@gmail.com>
2019-05-25 17:52:23 +0430

    Add home screen

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 
 // Views
+import Home from './views/Home.vue';
 import Signup from './views/Signup.vue';
 import Signin from './views/Signin.vue';
 import MovieDetails from './views/MovieDetails.vue';
@@ -12,6 +13,7 @@ export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
+    { path: '/', component: Home },
     { path: '/signup', component: Signup },
     { path: '/signin', component: Signin },
     { path: '/movies/:id', component: MovieDetails },
