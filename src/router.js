a746cb19da4ae820a2437a11c19f08639e1687b0
ryakosh <ryakosh@gmail.com>
2019-06-23 12:07:49 +0430

    Remove unused views

--- src/router.js
+++ src/router.js
@@ -3,8 +3,6 @@ import Router from "vue-router";
 
 // Views
 import Home from "./views/Home.vue";
-import Signup from "./views/Signup.vue";
-import Signin from "./views/Signin.vue";
 import MovieDetails from "./views/MovieDetails.vue";
 
 Vue.use(Router);
@@ -14,8 +12,6 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     { path: "/", component: Home },
-    { path: "/signup", component: Signup },
-    { path: "/signin", component: Signin },
     { path: "/movies/:id", name: "movie", component: MovieDetails }
   ]
 });
